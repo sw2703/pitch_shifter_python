f51c5d5c94a0f44fe0950c3551a4ccb5e34853a5
sw2703 <30507015+sw2703@users.noreply.github.com>
2023-03-24 06:52:58 +0800

    Update change_pitch.py
    Fix sign error.

--- change_pitch.py
+++ change_pitch.py
@@ -5,11 +5,11 @@ def change_pitch(input_file, output_file, semitones):
     sound = AudioSegment.from_file(input_file, format="mp3")
 
     # Use time stretching to adjust the playback speed without changing the pitch
-    sound_tempo = sound.speedup(playback_speed=(2.0 ** (semitones/12.0)), chunk_size=150)
+    sound_tempo = sound.speedup(playback_speed=(2.0 ** (-semitones/12.0)), chunk_size=150)
 
     # Shift the pitch up or down by the specified number of semitones
     sound_shifted = sound_tempo._spawn(sound_tempo.raw_data, overrides={
-        "frame_rate": int(sound_tempo.frame_rate * (2.0 ** (-semitones/12.0)))
+        "frame_rate": int(sound_tempo.frame_rate * (2.0 ** (semitones/12.0)))
     })
 
     # Export the output audio file
