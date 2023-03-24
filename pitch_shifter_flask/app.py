179ac7eb2febf3d2bf10447c38c73a53cf8b548c
sw2703 <30507015+sw2703@users.noreply.github.com>
2023-03-25 07:25:31 +0800

    Update app.py
    Download mp3 file at a lowered pitch.

--- pitch_shifter_flask/app.py
+++ pitch_shifter_flask/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
+from change_pitch_from_url import change_pitch_from_url
 
 app = Flask(__name__)
 CORS(app)  # This enables CORS for all routes in your Flask app
@@ -8,12 +9,9 @@ CORS(app)  # This enables CORS for all routes in your Flask app
 def run_python_function():
     data = request.json
     input_value = data.get('input_value', '')
-    result = your_python_function(input_value)
-    return jsonify(result=result)
-
-def your_python_function(input_value):
-    with open(r'C:\Users\sw270\Downloads\hello_world.txt', 'w') as file:
-        file.write(input_value)
+    output_file = 'hello_world.mp3'
+    change_pitch_from_url(input_value, output_file, -2)
+    return send_file(output_file, mimetype='audio/mpeg', as_attachment=True, download_name=output_file)
 
 if __name__ == '__main__':
     app.run(debug=True)
