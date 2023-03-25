d33a3476db68e13030ce29a3661cc41a387e374b
sw2703 <30507015+sw2703@users.noreply.github.com>
2023-03-25 17:41:17 +0800

    Update background.js

--- pitch_shifter_plugin/background.js
+++ pitch_shifter_plugin/background.js
@@ -1,10 +1,19 @@
 chrome.runtime.onInstalled.addListener(() => {
+  // Option to play at -2 semitones
   chrome.contextMenus.create({
-    title: 'Call Python Function',
+    title: 'Play at -2 Semitones',
     contexts: ['link'],
     id: 'callPythonFunction'
   });
+
+  // Option to play at -4 semitones
+  chrome.contextMenus.create({
+    title: 'Play at -4 Semitones',
+    contexts: ['link'],
+    id: 'callPythonFunctionMinus4'
+  });
 });
+
 
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
   if (info.menuItemId === 'callPythonFunction') {
@@ -18,6 +27,45 @@ chrome.contextMenus.onClicked.addListener(async (info, tab) => {
     });
 
     const data = await response.json();
-    console.log('Python function result:', data.result);
+    const audioBlob = base64ToBlob(data.mp3_base64, 'audio/mpeg');
+
+    // Play the audio
+    const audioUrl = URL.createObjectURL(audioBlob);
+    const audioPlayerHtml = `
+      <html>
+        <body>
+          <audio controls autoplay>
+            <source src="${audioUrl}" type="audio/mpeg">
+          </audio>
+        </body>
+      </html>
+    `;
+    const audioPlayerUrl = 'data:text/html,' + encodeURIComponent(audioPlayerHtml);
+    chrome.tabs.create({ url: audioPlayerUrl });
+   
+
+    // Download the MP3 file
+    const downloadUrl = URL.createObjectURL(audioBlob);
+    chrome.downloads.download({
+      url: downloadUrl,
+      filename: data.download_name
+    });
   }
 });
+
+function base64ToBlob(base64, mimeType) {
+  const byteCharacters = atob(base64);
+  const byteArrays = [];
+
+  for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+    const slice = byteCharacters.slice(offset, offset + 512);
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    byteArrays.push(byteArray);
+  }
+
+  return new Blob(byteArrays, { type: mimeType });
+}
